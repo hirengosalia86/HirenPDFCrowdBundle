68c8d163aa4761fe5d5a6e4bd02f11f4cebaa3d0
hirengosalia86 <hirengosalia86@gmail.com>
2016-04-14 11:25:26 -0400

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Amp\PDFCrowdBundle\DependencyInjection;
+namespace Hiren\PDFCrowdBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('amp_pdf_crowd');
+        $rootNode = $treeBuilder->root('hiren_pdf_crowd');
 
         $rootNode
             ->children()
