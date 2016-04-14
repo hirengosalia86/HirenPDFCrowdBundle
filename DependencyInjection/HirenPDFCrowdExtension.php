acbe3c1daddee531e65243bf53812a3987c59309
hirengosalia86 <hirengosalia86@gmail.com>
2016-04-14 11:26:21 -0400

    Update and rename AmpPDFCrowdExtension.php to HirenPDFCrowdExtension.php

--- DependencyInjection/HirenPDFCrowdExtension.php
+++ DependencyInjection/HirenPDFCrowdExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Amp\PDFCrowdBundle\DependencyInjection;
+namespace Hiren\PDFCrowdBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class AmpPDFCrowdExtension extends Extension
+class HirenPDFCrowdExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -22,8 +22,8 @@ class AmpPDFCrowdExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('amp_pdf_crowd.username', $config['username']);
-        $container->setParameter('amp_pdf_crowd.apikey', $config['apikey']);
+        $container->setParameter('hiren_pdf_crowd.username', $config['username']);
+        $container->setParameter('hiren_pdf_crowd.apikey', $config['apikey']);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
